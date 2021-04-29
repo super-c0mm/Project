5d9a38f79a36ef39d3882dc1fa5356446bb69330
super-c0mm <51075560+super-c0mm@users.noreply.github.com>
2021-04-29 22:30:32 +0300

    Add files via upload

---
+++ Affiliates/JS/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+$("a[href*=#]").on("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 777);
+		e.preventDefault();
+		return false;
+	});
+});
